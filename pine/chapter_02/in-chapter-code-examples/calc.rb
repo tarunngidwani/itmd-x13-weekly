e56f438807561c10aef944437043acafd07e6c63
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-05-25 19:51:48 -0500

    Added complex arithmetic using parentheses example for chap 2

--- pine/chapter_02/in-chapter-code-examples/calc.rb
+++ pine/chapter_02/in-chapter-code-examples/calc.rb
@@ -1,11 +1,24 @@
 # Chap 2: Page 9
-puts 1+2
+puts 1 + 2
 
 puts
 
-# Chapter 2: Simple Arithmetic with floats, Page 10
+# Chap 2: Simple Arithmetic with floats, Page 10
 puts 1.0 + 2.0
 puts 2.0 * 3.0
 puts 5.0 - 8.0
 puts 9.0 / 2.0
 
+puts
+
+# Chap 2: Simple Arithmetic with integers, Page 11
+puts 1 + 2
+puts 2 * 3
+puts 5 - 8
+puts 9 / 2
+
+puts
+
+# Chap 2: More Complex Arithmetic Expressions using parenthesis, Page 11
+puts 5 * (12 - 8) + -15
+puts 98 + (59_872 / (13 * 8)) * -51
