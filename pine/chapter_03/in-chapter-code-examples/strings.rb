310198bd683f6966c5646d67cc245fca601d4393
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-05-25 20:26:12 -0500

    Added example about numbers vs digits for chap 3

--- pine/chapter_03/in-chapter-code-examples/strings.rb
+++ pine/chapter_03/in-chapter-code-examples/strings.rb
@@ -14,3 +14,14 @@ puts
 
 # Chap 3: String Multiplication, Page 14
 puts 'blink ' * 4
+
+puts
+
+# Chap 3: Numbers vs Digits, Page 14
+puts 12   + 12
+puts '12' + '12'
+puts '12 + 12'
+puts
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
