159ade992d623450c70b373677da01445e84e3f2
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-05-25 20:03:54 -0500

    Added string addition and multiplication examples for chap 3

--- pine/chapter_03/in-chapter-code-examples/strings.rb
+++ pine/chapter_03/in-chapter-code-examples/strings.rb
@@ -1,4 +1,16 @@
-# Chap 3:Printing Strings, Page 13
+# Chap 3: Printing Strings, Page 13
 puts'Hello, world!'
 puts ''
 puts'Good-bye.'
+
+puts
+
+# Chap 3: String Arithmetic, Page 13-14
+puts 'I like'  + 'apple pie.'
+puts 'I like ' + 'apple pie.'
+puts 'I like'  + ' apple pie.'
+
+puts
+
+# Chap 3: String Multiplication, Page 14
+puts 'blink ' * 4
