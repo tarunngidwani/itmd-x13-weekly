e92ff040071e50330bf63293918ade712ae13bbe
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-05-27 13:41:42 -0500

    Added gets and chomp example from ch 5

---
+++ pine/chapter_05/gets_and_chomp.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# Chapter 5
+
+# The gets method, Page 23
+puts gets
+
+puts
+
+# The chomp method, Page 24
+# Interactive program
+# No chomp, gets the carriage return along with string entered
+puts 'Hello there, and what\'s your name?'
+name = gets
+puts 'Your name is ' + name + '? What a lovely name!'
+puts 'Pleased to meet you, ' + name + '.  :)'
+
+puts
+
+# Using chomp method with gets, gets rid of the carriage return
+# at the end of the string
+puts 'Hello there, and what\'s your name?'
+name = gets.chomp
+puts 'Your name is ' + name + '? What a lovely name!'
+puts 'Pleased to meet you, ' + name + '.  :)'
