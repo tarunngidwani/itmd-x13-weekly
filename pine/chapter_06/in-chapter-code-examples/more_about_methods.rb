06d1c276afd508ca347a2080577c01f5f2dc92a5
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-05-27 14:35:32 -0500

    Added string's reverse and lenght method examples from ch 6

--- pine/chapter_06/in-chapter-code-examples/more_about_methods.rb
+++ pine/chapter_06/in-chapter-code-examples/more_about_methods.rb
@@ -16,3 +16,28 @@ puts
 
 # Shows what object you are in
 puts self
+
+puts
+
+# Fancy String Methods, Page 28
+
+# -> reverse: returns reversed version of string
+#             does not change the original string
+var1 = 'stop'
+var2 = 'deliver repaid desserts'
+var3 = '....TCELES B HSUP A magic spell?'
+
+puts var1.reverse
+puts var2.reverse
+puts var3.reverse
+puts var1
+puts var2
+puts var3
+
+puts
+
+# -> length: tells us number of characters in a string
+puts 'What is you full name?'
+name = gets.chomp
+puts 'Did you know there are ' + name.length.to_s + ' characters'
+puts 'in your name, ' + name + '?'
