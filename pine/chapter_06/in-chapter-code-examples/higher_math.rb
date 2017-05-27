9e34352af4fac4ab6caec197deb1a3bba16eaade
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-05-27 15:28:23 -0500

    Added more arithmetic examples in higher math section from ch 6

---
+++ pine/chapter_06/in-chapter-code-examples/higher_math.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Chapter 6
+
+# More Arithmetic, Page 32-33
+
+# **: exponent
+# %: modulus
+puts 5**2
+puts 5**0.5
+puts 7 / 3
+puts 7 % 3
+puts 7.0 % 3.0
+puts 365 % 7
+
+puts
+
+# abs: returns absolute value of the number
+puts((5 - 2).abs)
+puts((2 - 5).abs)
