202fd2d03a6b60f591473394af033220621bbef2
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-05-27 23:17:18 -0500

    Added second loop to get user input example from ch 7

--- pine/chapter_07/in-chapter-code-examples/looping.rb
+++ pine/chapter_07/in-chapter-code-examples/looping.rb
@@ -1,12 +1,24 @@
 # frozen_string_literal: true
 
 # Chapter 7
-# Looping, Page 42
+# Looping, Page 42-43
 
 # Loop #1: to get user input
 input = ''
+puts 'Loop 1:'
 until input.downcase == 'bye'
   puts input
   input = gets.chomp
+end
+puts 'Come again soon!'
+
+puts
+
+# Loop #2: to get user input
+puts 'Loop 2:'
+while 'Spike' > 'Angel'
+  input = gets.chomp
+  puts input
+  break if input.downcase == 'bye'
 end
 puts 'Come again soon!'
