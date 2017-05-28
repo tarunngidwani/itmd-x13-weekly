21ffb46588e692d544f1090665ebb1f853afb0b0
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-05-28 00:02:37 -0500

    Added logical operators example from ch 7

--- pine/chapter_07/in-chapter-code-examples/logic.rb
+++ pine/chapter_07/in-chapter-code-examples/logic.rb
@@ -37,3 +37,26 @@ name = gets.chomp.capitalize
 puts "Hello, #{name}."
 
 puts 'What a lovely name!' if %w[Chris Katy].include?(name)
+
+puts
+
+# Logical operators
+i_am_chris = true
+i_am_purple = false
+i_like_beer = true
+i_eat_rocks = false
+
+puts i_am_chris  && i_like_beer
+puts i_like_beer && i_eat_rocks
+puts i_am_purple && i_like_beer
+puts i_am_purple && i_eat_rocks
+puts
+
+puts i_am_chris  || i_like_beer
+puts i_like_beer || i_eat_rocks
+puts i_am_purple || i_like_beer
+puts i_am_purple || i_eat_rocks
+puts
+
+puts !i_am_purple
+puts !i_am_chris
