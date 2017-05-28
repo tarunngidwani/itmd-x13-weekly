f71dd10e94ddba33f486b6db3fe52c198bcd66e7
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-05-28 13:22:38 -0500

    Added method creation example from ch 9

---
+++ pine/chapter_09/in-chapter-code-examples/methods.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Chapter 9
+# Methods, Page 60-65
+
+# Creating methods
+def say_moo
+  puts 'mooooooo...'
+end
+
+2.times { say_moo }
+puts 'coin-coin'
+2.times { say_moo }
