1b7aece72bac24a5a0819f14359a83e9b1510124
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-05-27 23:50:41 -0500

    Added example that eliminates duplication of some code from ch 7

--- pine/chapter_07/in-chapter-code-examples/logic.rb
+++ pine/chapter_07/in-chapter-code-examples/logic.rb
@@ -28,3 +28,12 @@ if name == 'Chris'
 elsif name == 'Katy'
   puts 'What a lovely name!'
 end
+
+puts
+
+# Removing duplication of code
+print 'Hello, what\'s your name? '
+name = gets.chomp.capitalize
+puts "Hello, #{name}."
+
+puts 'What a lovely name!' if %w[Chris Katy].include?(name)
