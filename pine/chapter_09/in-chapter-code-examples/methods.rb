373d5559b992be357c1dfb65b07cd2a7b8485619
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-05-28 13:27:23 -0500

    Added method parameters example from ch 9

--- pine/chapter_09/in-chapter-code-examples/methods.rb
+++ pine/chapter_09/in-chapter-code-examples/methods.rb
@@ -11,3 +11,15 @@ end
 2.times { say_moo }
 puts 'coin-coin'
 2.times { say_moo }
+
+puts
+
+# Method parameters
+def say_moo(number_of_moos)
+  puts 'mooooooo...' * number_of_moos
+end
+
+say_moo 3
+puts 'oink-oink'
+# Error: Wrong number of arguments (0 of 1)
+# say_moo
