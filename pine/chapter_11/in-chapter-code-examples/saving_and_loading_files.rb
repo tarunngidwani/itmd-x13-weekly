648aa657ac39381bfe688e00d5be7b9068266b30
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-05-28 18:27:41 -0500

    Added yaml file interaction example from ch 11 along with resulting output txt file

--- pine/chapter_11/in-chapter-code-examples/saving_and_loading_files.rb
+++ pine/chapter_11/in-chapter-code-examples/saving_and_loading_files.rb
@@ -13,3 +13,23 @@ read_string = File.read filename
 puts read_string == test_string
 
 puts
+
+# Yaml, Page 86-87
+require 'yaml'
+
+test_array = [
+  'Give Quiche A Chance',
+  'Mutants Out!',
+  'Chameleonic Life-Forms, No Thanks'
+]
+
+test_string = test_array.to_yaml
+filename = 'RimmerTShirts.txt'
+
+File.open(filename, 'w') { |file_handle| file_handle.write test_string }
+
+read_string = File.read filename
+read_array = YAML.safe_load read_string
+
+puts read_string == test_string
+puts read_array == test_array
