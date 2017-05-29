b1636f7af990475f00594b0a8f70ec9931667f71
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-05-28 23:50:15 -0500

    Added double quoted vs single quoted string printing and escaping example

--- pine/chapter_11/in-chapter-code-examples/double_quoted_strings.rb
+++ pine/chapter_11/in-chapter-code-examples/double_quoted_strings.rb
@@ -17,3 +17,22 @@ puts buffy_quote_two
 
 puts
 puts buffy_quote_one == buffy_quote_two
+
+puts
+
+# Printing double quoted vs single quoted strings
+# - Double quotes: allow indentation of strings so
+#                  so they line up.
+# - Can put a multiline string on one line of code
+
+puts "3...\n2...\n1...\nHAPPY NEW YEAR!"
+puts
+# Does not work with simpler single-quoted strings
+puts '3...\n2...\n1...\nHAPPY NEW YEAR!'
+
+puts
+
+# - Escape single quotes in single-quoted strings (but not double quotes)
+puts 'single (\') and double (") quotes'
+# - Escape double quotes in double-quoted strings (but not single quotes)
+puts "single (') and double (\") quotes"
