4f06da14b1632d15ac514596f2107068339f3a62
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-05-29 12:41:29 -0500

    Created adding method to Integer class example from ch 13

---
+++ pine/chapter_13/in-chapter-code-examples/integer.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# Chapter 13
+# Changing Existing Classes, Page 103
+
+# - Before whenever we executed any code or defined any methods,
+#   we did it in the default program object: main
+# - Here we left that object and hopped into the already existing
+#   Integer class, defined a method there (making it an integer method)
+#   and now all integers can use it
+# - This can be done with any method in any class, even the
+#   built-in methods
+
+class Integer
+
+  def to_eng
+    num = 5
+    num == self ? 'five' : 'forty-two'
+  end
+end
+
+puts 5.to_eng
+puts 42.to_eng
