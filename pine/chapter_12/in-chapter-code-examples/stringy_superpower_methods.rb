0ffa299e46bcfc8848398675bdb8251576e64c24
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-05-29 02:56:31 -0500

    Added substring using [] method example from ch 12

--- pine/chapter_12/in-chapter-code-examples/stringy_superpower_methods.rb
+++ pine/chapter_12/in-chapter-code-examples/stringy_superpower_methods.rb
@@ -23,3 +23,30 @@ if name[0] == 'C'
   puts 'You have excellent taste in footwear.'
   puts 'I can just tell.'
 end
+
+puts
+
+# Substrings in two ways, instead of picking out
+# only one character
+# - Pass in two numbers:
+#    -> First tells where to start the substring
+#    -> Second tells us how long of a substring
+#       we are looking for
+prof = 'We tore the universe a new space-hole, alright!'
+puts prof[12, 8]
+
+# - Pass in a range
+#     -> Starting_Char_Index..Ending_Char_Index
+puts prof[12..19] # 8 chars total
+
+puts
+
+# Both ways have a twist: if you pass in a
+# negative index, it counts from the end
+# of the string
+def avi?(file_name)
+  file_name.downcase[-4..-1] == '.avi'
+end
+
+puts avi? 'DANCEMONKEYBOY.AVI'
+puts avi? 'toilet_paper_fiasco.jpg'
