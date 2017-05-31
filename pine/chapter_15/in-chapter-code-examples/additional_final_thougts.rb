0f7f41d4c0c866a49c5cfef359c80d5653bc1523
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-05-31 09:55:38 -0500

    Added branching shortcut syntax example and use of unless example from ch 15

---
+++ pine/chapter_15/in-chapter-code-examples/additional_final_thougts.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Chapter 15
+# For help:
+#          - irb (Interactive Ruby)
+#          - The Pickaxe: Programming Ruby 1.9 and 2.0
+#          - Ruby-Talk: The Ruby Mailing List
+
+# Ruby has the concept of TMTOWTDI which means:
+# There's More Than One Way To Do It
+
+#  - Makes things more expressive
+#  - Can be challenging to teach someone how to program
+#      |-> Learning one way to do something is challenging and confusing enough
+
+# String: 5 ways to create one: "", String.new, %q{}
+
+
+# Branching shortcut
+# if and unless
+# unless: executes code if conditional is false
+
+puts 'combergearl thememberate' if     5 == 2**2 + 1**1
+puts 'supposine follutify'      unless 'Chris'.length == 5
