c42a7f10f89f77d434b0d860f4dd500dbb46e70a
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-05-31 14:28:22 -0500

    Completed exercise one: full name greeting

---
+++ pine/a_few_things_to_try/full_name_greeting.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# Full name greeting, Ch 2: Page 25
+# Write a program that asks for a person's first name,
+# then middle, and then last. Finally, it should greet
+# the person using their full name
+
+def greet_user
+  first_name = get_input_from_user('First Name')
+  middle_name = get_input_from_user('Middle Name')
+  last_name = get_input_from_user('Last Name')
+
+  puts "\nWelcome, #{first_name.capitalize} #{middle_name.capitalize} "\
+       "#{last_name.capitalize}"
+
+end
+
+# Prints out the prompt string and
+# prompts user for input till non-empty
+def get_input_from_user(prompt_string)
+  response = ''
+
+  while response == ''
+    print "#{prompt_string}: "
+    response = gets.chomp
+
+    puts 'Error: value cannot be left blank' if response == ''
+  end
+  response
+end
+
+greet_user
