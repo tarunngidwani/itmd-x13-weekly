b988b0c658d28b19aba205c6bc62a761ed4a4b98
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-06-06 15:47:40 -0500

    Add unit test for image_url of product model
    - Add tests to ensure that the image_url for a given model
      ends with one of: .gif, .jpg, or .png

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -12,9 +12,9 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test 'product price must be positive' do
-    product = Product.new(title: 'My Book Title',
-                             description:   'yyy',
-                             image_url:     'zzz.jpg')
+    product = Product.new(title:         'My Book Title',
+                          description:   'yyy',
+                          image_url:     'zzz.jpg')
     expected_err_msg = 'must be greater than or equal to 0.01'
 
     product.price = -1
@@ -30,5 +30,28 @@ class ProductTest < ActiveSupport::TestCase
     product.price = 1
     assert product.valid?
     assert_equal [], product.errors[:price]
+  end
+
+  def new_product(image_url)
+    Product.new(title:       'My Book Title',
+                description: 'yyy',
+                price:       1,
+                image_url:   image_url)
+  end
+
+  test 'image url must end with one of: gif, jpg, or png' do
+    ok = %w[fred.gif fred.jpg fred.png FREG.JPG FRED.Jpg
+            http://a.b.c/x/y/z/fred.gif]
+    bad = %w[fred.doc fred.gif/more fred.gif.more]
+
+    ok.each do |image_url|
+      assert (new_product image_url).valid?,
+             "#{image_url} shouldn't be invalid"
+    end
+
+    bad.each do |image_url|
+      assert (new_product image_url).invalid?,
+             "#{image_url} shouldn't be valid"
+    end
   end
 end
