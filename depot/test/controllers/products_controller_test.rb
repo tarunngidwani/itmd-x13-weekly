29be4e0da4c5e74089fdf093340be3e9f4ea65a5
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-06-06 13:28:34 -0500

    Fix should create product failing test
    Create a valid test product object to pass to post product_url
    to ensure that a new product is created
      - This will fix the failing test as the condition increase in
        product count is met with this product being created

--- depot/test/controllers/products_controller_test.rb
+++ depot/test/controllers/products_controller_test.rb
@@ -3,6 +3,12 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @update = {
+      title:       'Lorem Ipsum',
+      description: 'Wibbles are fun!',
+      image_url:   'lorem.jpg',
+      price:        19.95
+    }
   end
 
   test "should get index" do
@@ -17,7 +23,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+      post products_url, params: { product: @update }
     end
 
     assert_redirected_to product_url(Product.last)
