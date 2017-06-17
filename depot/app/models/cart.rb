9305e539b231dfcea49c70f22d49a84c13bb78d4
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-06-16 22:35:50 -0500

    Create method to add or update a line item
        - When the add to cart button is clicked for a
          specific product the line_items#create action
          is invoked
        - This function checks if there is a line_item for the
          product to be added to the cart:
            |-> If that line_item refernces said product the
                quantity of the line_item is incremented by 1
            |-> Else a new line_item is created with default
                quantity 1, which references the product to be
                added and that line_item is added to the cart
                of the current session

--- depot/app/models/cart.rb
+++ depot/app/models/cart.rb
@@ -1,3 +1,14 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+
+  def add_or_update_line_item(product_id, cart_id)
+    current_line_item = line_items.find_by(product_id: product_id)
+
+    if current_line_item
+      current_line_item.quantity += 1
+    else
+      current_line_item = LineItem.new(product_id: product_id, cart_id: cart_id)
+    end
+    current_line_item
+  end
 end
