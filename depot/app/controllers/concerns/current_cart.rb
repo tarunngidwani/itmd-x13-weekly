1ebbf9c302421045329782f610a198eeea2ba6e3
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-06-16 23:27:08 -0500

    Move add or update line item function from cart model to current cart concern

--- depot/app/controllers/concerns/current_cart.rb
+++ depot/app/controllers/concerns/current_cart.rb
@@ -8,4 +8,15 @@ module CurrentCart
     @cart = Cart.create
     session[:cart_id] = @cart.id
   end
+
+  def add_or_update_line_item(product_id, cart_id)
+    current_line_item = LineItem.all.find_by(product_id: product_id)
+
+    if current_line_item
+      current_line_item.quantity += 1
+    else
+      current_line_item = LineItem.new(product_id: product_id, cart_id: cart_id)
+    end
+    current_line_item
+  end
 end
