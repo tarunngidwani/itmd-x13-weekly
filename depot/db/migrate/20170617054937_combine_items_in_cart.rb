3c12784deb4d2fc2a4cc49f7a2634977057478bc
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-06-17 13:51:04 -0500

    Create migration to combine items in cart
          - Create a migration that will replace multiple line items
            referencing a single product in a cart with a single
            line item

--- depot/db/migrate/20170617054937_combine_items_in_cart.rb
+++ depot/db/migrate/20170617054937_combine_items_in_cart.rb
@@ -1,4 +1,24 @@
 class CombineItemsInCart < ActiveRecord::Migration[5.1]
-  def change
+  def change; end
+
+  def up
+    # replace multiple items for a single product in a cart with a
+    # single item
+    Cart.all.each do |cart|
+      # count the number of each product in cart
+      sums = cart.line_items.group(:product_id).sum(:quantity)
+
+      sums.each do |product_id, quantity|
+        next unless quantity > 1
+
+        # remove individual items
+        cart.line_items.where(product_id: product_id).delete_all
+
+        # replace with single item
+        line_item = cart.line_items.build(product_id: product_id,
+                                          quantity: quantity)
+        line_item.save!
+      end
+    end
   end
 end
