f6e297bc8a99c685a5b77d728ed54ccbb6fde897
Tarun Gidwani <tgidwani@hawk.iit.edu>
2017-06-20 14:55:33 -0500

    Create private method in Orders controller
      - Add private method called ensure cart isnt empty
        which checks that if the number of line items
        in current cart for the session is zero.
      - If that is the case then it redirects the user to
        the store catalog page and displays a notice
        indicating to the user that their cart is empty

--- depot/app/controllers/orders_controller.rb
+++ depot/app/controllers/orders_controller.rb
@@ -71,4 +71,10 @@ class OrdersController < ApplicationController
     def order_params
       params.require(:order).permit(:name, :address, :email, :pay_type)
     end
+
+    def ensure_cart_isnt_empty
+      if @cart.line_items.empty?
+        redirect_to store_index_url, notice: 'Your cart is empty'
+      end
+    end
 end
